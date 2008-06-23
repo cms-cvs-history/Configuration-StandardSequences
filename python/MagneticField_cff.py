c455edda3a64b342fa3cd107b14933ed14ca5058
cmsbuild <>
2008-06-23 19:06:09 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- StandardSequences-V01-07-15'.
    Sprout from master 2008-06-23 19:06:07 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'remove dependency on random engine definitions'
    Cherrypick from master 2008-06-16 21:26:23 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'dedicated standard files for each configuration of magnetic field. These files replace old volumeBasedMagneticField.cfi which is now turned into a dummy file. only these files should be used in high level py':
        data/HLTtable.cff
        python/MagneticField_cff.py

--- python/MagneticField_cff.py
+++ python/MagneticField_cff.py
@@ -4,3 +4,5 @@ import FWCore.ParameterSet.Config as cms
 # Master configuration for the magnetic field
 # old mapping with 4T
 from Configuration.StandardSequences.MagneticField_40T_851_cff import *
+
+
