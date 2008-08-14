7df10791d188be926524e4a2ee0645573053c20e
cmsbuild <>
2008-08-14 12:02:56 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- StandardSequences-V01-07-37'.
    Sprout from master 2008-07-30 01:14:58 UTC Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch> 'removed GT EVM unpacker as temporary measure'
    Cherrypick from master 2008-08-14 12:02:55 UTC David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch> 'version of FrontierConditions_GlobalTag_cff without forcing es_prefer for GlobalTag':
        python/AlCaRecoStreams_cff.py
        python/FrontierConditions_GlobalTag_cff.py
        python/FrontierConditions_GlobalTag_noesprefer_cff.py
    Cherrypick from master 2008-08-01 11:59:49 UTC Shahram Rahatlou <sha1-ca1514f63de30935a6ecb085b6242dc5bad3fd8f@cern.ch> 'define RawToDigi_woGCT temporarily':
        python/RawToDigi_Data_cff.py
    Cherrypick from master 2008-08-14 08:28:12 UTC David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch> 'use ALCARECOHcalCalIsoTrk instead of ALCARECOHcalCalIsoTrkNoHLT for data taking':
        python/AlCaReco_cff.py

--- python/FrontierConditions_GlobalTag_cff.py
+++ python/FrontierConditions_GlobalTag_cff.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
+
 # the following is needed for non PoolDBESSources (fake calibrations)
 #
-from CalibTracker.SiStripESProducers.SiStripPedestalsFakeSource_cfi import *
-from CalibTracker.SiStripESProducers.SiStripQualityFakeESSource_cfi import *
 from CalibCalorimetry.EcalLaserCorrection.ecalLaserCorrectionService_cfi import *
-from CalibTracker.SiStripESProducers.SiStripGainESProducer_cfi import *
-from RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi import *
-from RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi import *
+from CalibTracker.Configuration.Tracker_DependentRecords_forGlobalTag_cff import *
+es_prefer_GlobalTag = cms.ESPrefer("PoolDBESSource","GlobalTag")
+
 hcal_db_producer = cms.ESProducer("HcalDbProducer",
     dump = cms.untracked.vstring(''),
     file = cms.untracked.string('')
@@ -18,9 +18,3 @@ es_hardcode = cms.ESSource("HcalHardcodeCalibrations",
         'channelQuality', 
         'ZSThresholds')
 )
-
-sistripconn = cms.ESProducer("SiStripConnectivity")
-
-# end fake calibrations
-
-from Configuration.StandardSequences.FrontierConditions_GlobalTag_cfi import *
