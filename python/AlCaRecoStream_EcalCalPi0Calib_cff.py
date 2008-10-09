e323b8f3566b774bf13b30045adca240502f3406
cmsbuild <>
2008-10-09 20:27:13 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- StandardSequences-V01-08-00'.
    Sprout from master 2008-07-23 14:33:50 UTC Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch> 'remove GT EVM from digi2raw'
    Cherrypick from master 2008-10-09 20:27:12 UTC David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch> 'adding end of process job - moving metoedm converter to that':
        python/AlCaRecoStream_EcalCalPhiSym_cff.py
        python/AlCaRecoStream_EcalCalPi0Calib_cff.py
        python/AlCaRecoStream_HcalCalIsoTrk_cff.py
        python/AlCaRecoStream_HcalCalMinBias_cff.py
        python/DigiCosmics_cff.py
        python/EndOfProcess_cff.py
        python/FrontierConditions_GlobalTag_noesprefer_cff.py
        python/MagneticField_0T_cff.py
        python/MagneticField_20T_cff.py
        python/MagneticField_30T_cff.py
        python/MagneticField_35T_cff.py
        python/SimNOBEAM_cff.py
        python/VtxSmearedEarly900GeVCollision_cff.py

---
+++ python/AlCaRecoStream_EcalCalPi0Calib_cff.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+# last update: $Date: 2008/09/16 20:35:58 $ by $Author: futyand $
+
+# ECAL calibration with pi0
+from Calibration.EcalAlCaRecoProducers.ALCARECOEcalCalPi0Calib_cff import *
+from RecoEcal.EgammaClusterProducers.geometryForClustering_cff import *
+from DQMOffline.Configuration.AlCaRecoDQM_cff import *
+
+pathALCARECOEcalCalPi0Calib = cms.Path(seqALCARECOEcalCalPi0Calib*ALCARECOEcalCalPi0CalibDQM)
+
+from Configuration.EventContent.AlCaRecoOutput_cff import *
+
+ALCARECOStreamEcalCalPi0Calib = cms.FilteredStream(
+	responsible = 'Vladimir Litvine',
+	name = 'ALCARECOEcalCalPi0Calib',
+	paths  = (pathALCARECOEcalCalPi0Calib),
+	content = OutALCARECOEcalCalPi0Calib.outputCommands,
+	selectEvents = OutALCARECOEcalCalPi0Calib.SelectEvents,
+	dataTier = cms.untracked.string('ALCARECO')
+	)
