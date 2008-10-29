7e4b9e95e388fc8d1a39d51c09228fefdc16294a
cmsbuild <>
2008-10-29 11:52:38 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- StandardSequences-V01-11-03'.
    Sprout from master 2008-10-29 11:52:37 UTC fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch> 'temporary solution for make the global valiation running on the 2nd step'
    Cherrypick from master 2008-04-21 01:10:10 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'second run of translating configs':
        python/L1TriggerDefaultMenu_cff.py

--- python/L1TriggerDefaultMenu_cff.py
+++ python/L1TriggerDefaultMenu_cff.py
@@ -2,4 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 # Choose a menu/prescale/mask from one of the .cffs
 # in L1TriggerConfig/L1GtConfigProducers/data/Luminosity
-from  L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1030.L1Menu_2008MC_2E30_Unprescaled_cff import *
+from L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1030.L1Menu2008_2E30_Unprescaled_cff import *
+
