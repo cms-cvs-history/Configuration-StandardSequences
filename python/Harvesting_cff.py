3fa319d56f0bc87cc65439aef7e8bd86519d1629
cmsbuild <>
2009-09-01 12:58:30 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- StandardSequences-V01-16-03-04'.
    Sprout from master 2009-09-01 12:58:24 UTC fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch> 'add the fix for the TrigPrim di ECAL'
    Cherrypick from master 2009-05-14 14:49:40 UTC fambrogl <sha1-e692d0cc07db26fe72a735bf800f9d70a3ebe9a2@cern.ch> 'remove the obsolete special PU postValidation sequence':
        python/Generator_cff.py
        python/Harvesting_cff.py
        python/L1TriggerDefaultMenu_cff.py
        python/Services_cff.py
    Cherrypick from master 2009-07-03 12:28:41 UTC Andrea Rizzi <sha1-1bc0dceaaf7943f784ebe0a9992d5584d5d83d2a@cern.ch> 'add log error harvester in cosmic sequence':
        python/ReconstructionCosmics_cff.py
    Cherrypick from master 2009-07-22 21:05:10 UTC Luca Malgeri <sha1-15add160ebd241c8d3e199122754bfa2fae2a8ba@cern.ch> 'Added muonIdValDQMSeq in validation_prod':
        python/Validation_cff.py
    Delete:
        python/ReconstructionHeavyIons_cff.py

--- python/Harvesting_cff.py
+++ python/Harvesting_cff.py
@@ -12,10 +12,6 @@ dqmHarvesting = cms.Path(DQMOffline_SecondStep*DQMOffline_Certification)
 
 validationHarvesting = cms.Path(postValidation*hltpostvalidation)
 
-validationpreprodHarvesting = cms.Path(postValidation*hltpostvalidation_preprod)
-
-validationprodHarvesting = cms.Path(postValidation*hltpostvalidation_prod)
-
 validationHarvestingFS = cms.Path(HarvestingFastSim)
 
 #alcaHarvesting = cms.Path()
