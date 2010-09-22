8c9af7c72309065ee6f19c4111a705b0a2837e38
cmsbuild <>
2010-09-22 14:45:02 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- StandardSequences-CMSSW_3_8_4_patch2'.
    Sprout from master 2010-09-15 13:20:52 UTC Fabio Cossutti <sha1-3dce45789e317cc29a75783a879c7e618b062b82@cern.ch> 'Fix the outputModules access'
    Cherrypick from master 2010-07-06 12:44:36 UTC Andreas Mussgiller <sha1-221a492635cb55c4151f2a5361bd86aeae15e99c@cern.ch> 'added streams for the cosmics in collision reconstruction sequence':
        python/AlCaRecoStreams_cff.py
    Cherrypick from master 2010-09-22 14:45:01 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'good col skim integration':
        python/Skims_cff.py
    Delete:
        python/AlCaRecoStreamsHeavyIons_cff.py
        python/GeometryGFlashDB_cff.py

--- python/Skims_cff.py
+++ python/Skims_cff.py
@@ -212,6 +212,21 @@ SKIMStreamGoodVtx = cms.FilteredStream(
     dataTier = cms.untracked.string('RAW-RECO')
     )
 
+#############
+
+from DPGAnalysis.Skims.goodcollSkim_cff import *
+pathgoodcoll1 = cms.Path(goodcollL1requirement)
+pathgoodcolhf = cms.Path(goodcollHFrequirement)
+
+SKIMStreamGoodCol = cms.FilteredStream(
+    responsible = 'PVT',
+    name = 'GoodCol',
+    paths = (goodvertexSkimPath,pathgoodcoll1,pathgoodcolhf),
+    content = skimContent.outputCommands,
+    selectEvents = cms.untracked.PSet(),
+    dataTier = cms.untracked.string('RAW-RECO')
+    )
+
 #####################
 
 from DPGAnalysis.Skims.muonTracksSkim_cff import *
@@ -238,6 +253,37 @@ SKIMStreamValSkim = cms.FilteredStream(
     selectEvents = cms.untracked.PSet(),
     dataTier = cms.untracked.string('RAW-RECO')
     )
+
+#####################
+
+from DPGAnalysis.Skims.WZEGSkim_cff import *
+WZEGSkimPath = cms.Path ( WZfilterSkim )
+SKIMStreamWZEG = cms.FilteredStream(
+    responsible = 'ECAL DPG & EGM POG',
+    name = 'WZEG',
+    paths = ( WZEGSkimPath ),
+    content = skimContent.outputCommands,
+    selectEvents = cms.untracked.PSet(),
+    dataTier = cms.untracked.string('RAW-RECO')
+    )
+
+
+
+#####################
+
+from DPGAnalysis.Skims.WZMuSkim_cff import *
+ZMuSkimPath      = cms.Path(diMuonSelSeq)
+WtcMetSkimPath   = cms.Path(tcMetWMuNuSeq)
+WpfMetSkimPath   = cms.Path(pfMetWMuNuSeq)
+SKIMStreamWZMu   = cms.FilteredStream(
+    responsible = 'Muon DPG-POG-MuEW',
+    name = 'WZMu',
+    paths = (ZMuSkimPath, WtcMetSkimPath, WpfMetSkimPath),
+    content = skimContent.outputCommands,
+    selectEvents = cms.untracked.PSet(),
+    dataTier = cms.untracked.string('RAW-RECO')
+    )
+
 
 #####################
 
